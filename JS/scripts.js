c957e2d8ec2f71ddd9ab6b4be9803d5a67462378
Ignacio N. Tártaro <86450461+SLNacho@users.noreply.github.com>
2021-11-13 18:45:17 -0300

    lo actualizo por si acaso

--- JS/scripts.js
+++ JS/scripts.js
@@ -20,10 +20,10 @@ calc = document.getElementById("calc");
 			imc.innerHTML = imcx
 			console.log(imcx);
 
-			if(imcx<18.5){ lectura.innerHTML = "Peso inferior al normal"; }
-			else if(imcx>=18.5 && imcx<=24.9){ lectura.innerHTML = "Peso normal"; }
-			else if(imcx>=25 && imcx<=29.9){ lectura.innerHTML = "Peso superior al normal"; }
-			else if(imcx>30){ lectura.innerHTML = "Obesidad"; }
+			if(imcx<18.5){ lectura.innerHTML = "Peso inferior al normal"; document.getElementById("resultado1").classList.remove("disable"); }
+			else if(imcx>=18.5 && imcx<=24.9){ lectura.innerHTML = "Peso normal"; document.getElementById("resultado2").classList.remove("disable"); }
+			else if(imcx>=25 && imcx<=29.9){ lectura.innerHTML = "Peso superior al normal"; document.getElementById("resultado3").classList.remove("disable"); }
+			else if(imcx>30){ lectura.innerHTML = "Obesidad"; document.getElementById("resultado4").classList.remove("disable"); }
 
 		}else{
 			alert("Debe ingresar peso y altura.")
